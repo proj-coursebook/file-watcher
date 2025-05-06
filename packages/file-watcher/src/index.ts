d56055007883cbe041c43d0a72feb9f44f4ae21b
Ali Madooei <alimadooei@gmail.com>
2025-05-06 12:31:36 -0400

    Enhance FileWatcher implementation by adding WatchOptions interface and defining watch method

--- packages/file-watcher/src/index.ts
+++ packages/file-watcher/src/index.ts
@@ -1,5 +1,9 @@
-import { FileWatcher } from "./types";
+import { FileWatcher, WatchOptions } from "./types";
 
-class FileWatcherImpl implements FileWatcher {}
+class FileWatcherImpl implements FileWatcher {
+  watch(options: WatchOptions, onChange: (path: string) => Promise<void>): void {
+    throw new Error("Method not implemented.");
+  }
+}
 
 export { FileWatcherImpl };
--- packages/file-watcher/src/types.ts
+++ packages/file-watcher/src/types.ts
@@ -1,3 +1,14 @@
-interface FileWatcher {}
+interface WatchOptions {
+  /** Source directory to watch */
+  source: string;
+  /** Patterns to watch, relative to source directory */
+  include?: string[];
+  /** Patterns to ignore, relative to source directory */
+  exclude?: string[];
+}
 
-export type { FileWatcher };
+interface FileWatcher {
+  watch(options: WatchOptions, onChange: (path: string) => Promise<void>): void;
+}
+
+export type { FileWatcher, WatchOptions };
